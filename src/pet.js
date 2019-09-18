0a4b25cd8c9fe7b2078ca5b9b64ae67351c82196
edenchemo <edenchemo00@gmail.com>
2019-09-18 11:20:25 +0100

    virtual-pet commit 3/4

--- src/pet.js
+++ src/pet.js
@@ -1,14 +1,26 @@
+const MAXIMUM_FITNESS = 10;
 function Pet(name) {
     this.name = name;
     this.age = 2;
-    this.hunger = 0;
     this.fitness = 10;
-
+    this.hunger = 0;
   }
+
   Pet.prototype.growUp = function() {
     this.age += 1;
-    this.hunger -= 5;
     this.fitness -= 3;
-  };
-  
+    this.hunger += 5;
+  }
+
+  Pet.prototype.walk = function() {
+    this.fitness += 4;
+  }
+
+  Pet.prototype.walk = function() {
+    if ((this.fitness + 4) <= MAXIMUM_FITNESS ) {
+      this.fitness += 4;
+    } else {
+      this.fitness = MAXIMUM_FITNESS;
+    }
+  }
 module.exports = Pet;
